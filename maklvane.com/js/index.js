9248e9bc924f50135bc2848cc563dcc992bfb5ad
Alan Johnson <awrjohnson@gmail.com>
2024-05-22 10:53:09 -0700

    Updated to add clickable images that change the BG

--- maklvane.com/js/index.js
+++ maklvane.com/js/index.js
@@ -1,5 +1,9 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+
+//console log
 console.log("hello");
 
+//slideshow
 let slideIndex = 0;
 showSlides();
 
@@ -14,3 +18,23 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+
+//change color on click
+
+  // Select the element
+  const colorBox = document.getElementById('changebg');
+  const bgchangeheader = document.getElementById('changebgheader')
+  
+  // Define the function that changes the background color
+  const changeBackgroundColor = () => {
+      const colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange'];
+      const randomColor = colors[Math.floor(Math.random() * colors.length)];
+      colorBox.style.backgroundColor = randomColor;
+  };
+  
+  // Add the click event listener to the element
+  colorBox.addEventListener('click', changeBackgroundColor);
+  bgchangeheader.addEventListener('click', changeBackgroundColor)
+
+});
